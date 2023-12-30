0aa8ace6d70b7b558ea59fb13762acbb9bf0e944
yangliu-1995 <yangliu-1995@github.com>
2023-12-31 02:50:33 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .binaryTarget(
             name: "MobileVLCKit",
             url: "https://github.com/yangliu-1995/VLC-iOS-SPM/releases/download/1.3.6.0b10/MobileVLCKit.xcframework.zip",
-            checksum: "18f24566c84614eec10a59baafed0ea03d450493b8d0c74052b3034538a064ab"
+            checksum: "c88b6abfd2708fef1e7ba9f909d8092b5e0c428b6f7eeda67a11a6d77d91c742"
         ),
     ]
 )
